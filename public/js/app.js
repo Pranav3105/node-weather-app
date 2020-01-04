2a444c246dd13e5fc51dfae8fc1df16f48b62fc3
Kumar, Pranav <pk0752@gmail.com>
2020-01-04 15:38:36 +0530

    removed console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side JS is loaded');
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const place = document.querySelector('#place')
